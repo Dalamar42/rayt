c668386ffcb750b3a8d4297ae486d4f2c83335b7
Thomas Maroulis <thomasmaroulis@gmail.com>
2019-04-02 21:59:15 +0100

    Make clippy a little happier

--- src/world/texture/perlin.rs
+++ src/world/texture/perlin.rs
@@ -27,49 +27,64 @@ pub fn perlin_turbulence(config: &NoiseConfig, point: &Vector, depth: u8) -> f64
 }
 
 fn perlin_noise(config: &NoiseConfig, point: &Vector) -> f64 {
-    let u = point.x() - point.x().floor();
-    let v = point.y() - point.y().floor();
-    let w = point.z() - point.z().floor();
+    let intra_pixel_distance_u = point.x() - point.x().floor();
+    let intra_pixel_distance_v = point.y() - point.y().floor();
+    let intra_pixel_distance_w = point.z() - point.z().floor();
 
-    let i = point.x().floor() as i64;
-    let j = point.y().floor() as i64;
-    let k = point.z().floor() as i64;
+    let corner_i = point.x().floor() as i64;
+    let corner_j = point.y().floor() as i64;
+    let corner_k = point.z().floor() as i64;
 
-    let mut c: [[[Colour; 2]; 2]; 2] = [[[Colour::new(0.0, 0.0, 0.0); 2]; 2]; 2];
+    let mut ran_matrix: [[[Colour; 2]; 2]; 2] = [[[Colour::new(0.0, 0.0, 0.0); 2]; 2]; 2];
     for di in 0..2 {
         for dj in 0..2 {
             for dk in 0..2 {
-                let idx_i = (i + di) & 255;
-                let idx_j = (j + dj) & 255;
-                let idx_k = (k + dk) & 255;
+                let idx_i = (corner_i + di) & 255;
+                let idx_j = (corner_j + dj) & 255;
+                let idx_k = (corner_k + dk) & 255;
 
                 let idx = config.perm_x[idx_i as usize]
                     ^ config.perm_y[idx_j as usize]
                     ^ config.perm_z[idx_k as usize];
 
-                c[di as usize][dj as usize][dk as usize] = config.ran[idx];
+                ran_matrix[di as usize][dj as usize][dk as usize] = config.ran[idx];
             }
         }
     }
 
-    perlin_interpolation(c, u, v, w)
+    perlin_interpolation(
+        ran_matrix,
+        intra_pixel_distance_u,
+        intra_pixel_distance_v,
+        intra_pixel_distance_w,
+    )
 }
 
-fn perlin_interpolation(c: [[[Colour; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
-    let uu = u * u * (3.0 - 2.0 * u);
-    let vv = v * v * (3.0 - 2.0 * v);
-    let ww = w * w * (3.0 - 2.0 * w);
+fn perlin_interpolation(
+    ran_matrix: [[[Colour; 2]; 2]; 2],
+    intra_pixel_distance_u: f64,
+    intra_pixel_distance_v: f64,
+    intra_pixel_distance_w: f64,
+) -> f64 {
+    // Compute hermite cubic to eliminate Mach bands
+    let uu = intra_pixel_distance_u * intra_pixel_distance_u * (3.0 - 2.0 * intra_pixel_distance_u);
+    let vv = intra_pixel_distance_v * intra_pixel_distance_v * (3.0 - 2.0 * intra_pixel_distance_v);
+    let ww = intra_pixel_distance_w * intra_pixel_distance_w * (3.0 - 2.0 * intra_pixel_distance_w);
 
     let mut accum = 0.0;
 
-    for (i, i_axis) in c.iter().enumerate() {
+    for (i, i_axis) in ran_matrix.iter().enumerate() {
         for (j, j_axis) in i_axis.iter().enumerate() {
             for (k, ran) in j_axis.iter().enumerate() {
                 let i = i as f64;
                 let j = j as f64;
                 let k = k as f64;
 
-                let weight = Colour::new(u - i, v - j, w - k);
+                let weight = Colour::new(
+                    intra_pixel_distance_u - i,
+                    intra_pixel_distance_v - j,
+                    intra_pixel_distance_w - k,
+                );
 
                 accum += Colour::dot(&ran, &weight)
                     * (i * uu + (1.0 - i) * (1.0 - uu))
